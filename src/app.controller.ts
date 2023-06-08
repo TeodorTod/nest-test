43b34e25221a594d5ac206c0768653f929157df7
Teodor Todorov <teodor.at.tod@gmail.com>
2023-06-08 22:44:44 +0300

    crud operations in controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Req, Delete, Patch } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/user')
+  showUser() {
+    return { name: 'Pesho', age: '20' }
+  }
+
+  @Post()
+  store(@Req() req: Request) {
+    return req.body;
+  }
+
+  @Patch('/:userId')
+  update(@Req() req: Request) {
+    return req.body;
+  }
+
+  @Get('/:userId')
+  getUser(@Param() params: { userId: number }) {
+    return params
+  }
+
+  @Delete('/:userId')
+  deleteUser(@Param() params: { userId: number }) {
+    return params
   }
 }
