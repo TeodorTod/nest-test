64a6471efd6e53b441dd586c595b1657165a007f
Teodor Todorov <teodor.at.tod@gmail.com>
2023-06-13 22:13:46 +0300

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestjs',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
