791d29a8047de73e99ec3362e484305fc906d95f
Teodor Todorov <teodor.at.tod@gmail.com>
2023-06-14 21:46:45 +0300

    commit

--- src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Param, Post, Req, Delete, Patch } from '@nestjs/common';
-import { AppService } from './app.service';
-import { Request } from 'express';
-
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) { }
-
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
-  imports: [
-    UserModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'nestjs',
-      entities: [],
-      synchronize: true,
-    }),
-  ],
+  controllers: [],
+  providers: [],
+  imports: [],
 })
 export class AppModule {}
